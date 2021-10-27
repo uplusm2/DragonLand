191c88fd7ed86cec2cf12424316c1a2d9c35b3ed
umingming <u_0112@naver.com>
2021-10-27 15:02:06 +0900

    employee info

---
+++ DragonLand/src/com/project/Main/Main.java
@@ -0,0 +1,85 @@
+package com.project.Main;
+
+import java.io.*;
+import java.util.*;
+
+
+
+public class Main {
+	private static Scanner scan;
+	static {
+		scan = new Scanner(System.in);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Dragon Land"); //임시
+		System.out.printf("🎈오늘 용용랜드는 %s합니다.🎈%n%n", getState());
+		boolean loop = true;
+		while(loop) {
+			menu();
+			System.out.print("👉 ");
+			String sel = scan.nextLine();
+			System.out.println();
+			
+			if(sel.equals("1")) {		//용용랜드 정보
+			}else if(sel.equals("2")){	//로그인
+			}else if(sel.equals("3")){	//회원가입
+			}else if(sel.equals("4")){	//ID,PW찾기
+			}else if(sel.equals("5")){	//종료
+				loop = false;
+			}else {
+				System.out.println("다시 입력해주세요.");
+				pause();
+			}
+		}
+	}
+
+	private static String getState() {
+		try {
+			
+			//티켓예매정보.txt 파일 참조
+			String path = "C:\\Users\\u_011\\git\\DragonLand\\DragonLand\\data\\티켓예매정보.txt";
+			BufferedReader reader = new BufferedReader(new FileReader(path));
+			
+			//오늘 날짜 YYYYMMDD 형태로
+			Calendar c = Calendar.getInstance();
+			String today = String.format("%tF", c).replace("-", "");
+			
+			//혼잡도 비교를 위한 변수
+			int total = 0;
+
+			//파일 한 줄씩 읽으며 오늘 날짜랑 티켓 예매일이 겹치면 total에 인원수 추가
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				String[] temp = line.split("■");
+				if(temp[1].equals(today)) {
+					int adult = Integer.parseInt(temp[2]);
+					int teen = Integer.parseInt(temp[3]);
+					int kid = Integer.parseInt(temp[4]);
+					total += adult + teen + kid;
+				}
+			}
+			reader.close();
+			
+			//500을 기준으로 혼잡도 return
+			return total < 500 ? "쾌적" : "혼잡";
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static void menu() {
+		System.out.println("1. 용용랜드 정보");
+		System.out.println("2. 로그인");
+		System.out.println("3. 회원가입");
+		System.out.println("4. ID/PW 찾기");
+		System.out.println("5. 종료");
+	}
+	
+	private static void pause() {
+		
+		System.out.println("(엔터를 누르면 메뉴로 이동합니다.)");
+		scan.nextLine();
+	}
+}
