5d04a4f8b33c092dc3f5bf9204bd2473459e5ed7
umingming <u_0112@naver.com>
2021-10-28 17:41:44 +0900

    add

--- DragonLand/src/com/project/data/Staff.java
+++ DragonLand/src/com/project/data/Staff.java
@@ -8,43 +8,10 @@ public class Staff {
 	private String phoneNum;
 	private String place;
 	
-	public String getSeq() {
-		return seq;
+	public Staff() {
+		this("","","","","","");
 	}
-	public void setSeq(String seq) {
-		this.seq = seq;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getAge() {
-		return age;
-	}
-	public void setAge(String age) {
-		this.age = age;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getPhoneNum() {
-		return phoneNum;
-	}
-	public void setPhoneNum(String phoneNum) {
-		this.phoneNum = phoneNum;
-	}
-	public String getPlace() {
-		return place;
-	}
-	public void setPlace(String place) {
-		this.place = place;
-	}
-	
+
 	public Staff(String seq, String name, String age, String address, String phoneNum, String place) {
 		this.seq = seq;
 		this.name = name;
@@ -54,8 +21,51 @@ public class Staff {
 		this.place = place;
 	}
 	
-	public Staff() {
-		this("","","","","","");
+	public String getSeq() {
+		return seq;
 	}
 	
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAge() {
+		return age;
+	}
+	
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	
+	public String getPlace() {
+		return place;
+	}
+	
+	public void setPlace(String place) {
+		this.place = place;
+	}
 }
