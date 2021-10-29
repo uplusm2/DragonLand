e53119839996a23470ee4ae86dbd5c51e498cc1c
songj <songj@DESKTOP-O2CBAJ0>
2021-10-29 12:30:28 +0900

    재업

--- DragonLand/src/com/project/data/AttractionReservation2.java
+++ DragonLand/src/com/project/data/AttractionReservation2.java
@@ -1,6 +1,6 @@
 package com.project.data;
 
-public class AttractionReservation {
+public class AttractionReservation2 {
 
 	
 	private String code;
@@ -10,7 +10,7 @@ public class AttractionReservation {
 	private String ReserveNums;
 	private String mNum;
 	
-	public AttractionReservation(String code, String mtDate, String date, String hour, String ReserveNums, String mNum) {
+	public AttractionReservation2(String code, String mtDate, String date, String hour, String ReserveNums, String mNum) {
 		this.code = code;
 		this.mtDate = mtDate;
 		this.date = date;
