93fd79fcd356ce792868bcf98d6f29e67ab20e95
umingming <u_0112@naver.com>
2021-10-29 14:48:59 +0900

    Static으로 변경

--- DragonLand/src/com/project/data/Path.java
+++ DragonLand/src/com/project/data/Path.java
@@ -1,17 +1,17 @@
 package com.project.data;
 
 public class Path {
-	public String userVoice = "data\\고객의소리.txt";
-	public String attractionReservation = "data\\어트랙션예약정보.txt";
-	public String monthlySales = "data\\월별매출정보.txt";
-	public String dailySales = "data\\일별매출정보.txt";
-	public String staff = "data\\직원정보.txt";
-	public String ticketReservation = "data\\티켓예매정보.txt";
-	public String user = "data\\회원정보.txt";
-	public String location = "data\\놀이공원시설위치.txt";
-	public String attraction = "data\\어트랙션정보.txt";
-	public String workPlace = "data\\직원근무지.txt";
-	public String ticket = "data\\티켓요금.txt";
-	public String facility = "data\\편의시설정보.txt";
-	public String card = "data\\카드정보.txt";
+	public static String userVoice = "data\\고객의소리.txt";
+	public static String attractionReservation = "data\\어트랙션예약정보.txt";
+	public static String monthlySales = "data\\월별매출정보.txt";
+	public static String dailySales = "data\\일별매출정보.txt";
+	public static String staff = "data\\직원정보.txt";
+	public static String ticketReservation = "data\\티켓예매정보.txt";
+	public static String user = "data\\회원정보.txt";
+	public static String location = "data\\놀이공원시설위치.txt";
+	public static String attraction = "data\\어트랙션정보.txt";
+	public static String workPlace = "data\\직원근무지.txt";
+	public static String ticket = "data\\티켓요금.txt";
+	public static String facility = "data\\편의시설정보.txt";
+	public static String card = "data\\카드정보.txt";
 }
