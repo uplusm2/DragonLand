73aea47e4c4a8d5f5dc551b0e6638600b4db5a14
syeon-k <77730248+syeon-k@users.noreply.github.com>
2021-10-31 20:29:18 +0900

    # WARNING: head commit changed in the meantime
    jkljkl

---
+++ DragonLand/src/com/project/findUser/FindUser.java
@@ -0,0 +1,158 @@
+package com.project.findUser;
+
+import java.io.*;
+import java.util.*;
+
+import com.project.data.*;
+import com.project.main.Load;
+import com.project.main.Main;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.project.data.User;
+
+
+public class FindUser {
+
+	private static ArrayList<User> list;
+
+	public static void main(String[] args) throws Exception {
+
+		findeUser();
+
+	}
+
+	public static void findeUser() throws Exception {
+
+//			System.out.println("Dragon Land"); //임시
+//			System.out.printf("🎈오늘 용용랜드는 %s합니다.🎈%n%n", Main.getState());
+		Scanner find = new Scanner(System.in);
+		String findName;
+		String findJumin;
+
+		// 로그인 메뉴
+
+		System.out.println("\t\t=============================");
+		System.out.println("\t\t\t  ID/PW 찾기");
+		System.out.println("\t\t=============================");
+		System.out.println("\t\t   (b를 누르면 뒤로 이동합니다.)");
+
+		System.out.print("                이름: ");
+		findName = find.nextLine();
+		if (findName.equalsIgnoreCase("b")) {
+			Main.main(null);
+		}
+		System.out.print("                주민번호: ");
+		findJumin = find.nextLine();
+		if (findJumin.equalsIgnoreCase("b")) {
+			Main.main(null);
+		}
+
+		int findCheck = findUserInfo(find, findName, findJumin);
+
+		
+
+		if (findCheck == 1) {
+
+			
+			list = Load.loadUser();
+		
+			
+			
+			for(User s : list) {
+			
+			// ID PW 알려줌
+			if (s.getName().equals(findName) && s.getJumin().equals(findJumin)) {
+			
+			System.out.println("\t\t=============================");	
+			System.out.println("\t\t"+findName+"님의 아이디와 비밀번호입니다.");
+			System.out.println("\t\t=============================");
+			System.out.println("\t\t아이디 : " + s.getId());
+			System.out.println("\t\t비밀번호 : " + s.getPw());
+			System.out.println();
+			break;
+		
+			}
+				
+			}
+			
+			// 찾고나서 로그인 or 메뉴 선택 
+//			System.out.println("1.로그인하러가기");
+//			System.out.println("2.메뉴");
+//			System.out.print("👉 ");
+//			
+//			String choice = find.nextLine();
+//			
+//			if(choice.equals("1")) {
+//				login.login();
+//			} else if(choice.equals("2")) {
+//				Main.main(null);
+//			}
+//			
+			
+
+			
+		
+			// 4번. 아이디 비밀번호 모두 틀림
+		} else if (findCheck == 2) {
+
+			// 아이디 X 비밀번호 X
+			System.out.println("이름과 주민등록번호가 틀렸습니다.");
+
+			System.out.print("다시 찾으시겠습니까?(y/n) : ");
+			String reFind = find.nextLine();
+
+			if (reFind.equalsIgnoreCase("y")) {
+				findeUser();
+			} else if (reFind.equalsIgnoreCase("n")) {
+				Main.main(null);
+			}
+		}
+		// 아이디 비밀번호 찾기 오류
+		else {
+			System.out.println("유효한 값이 아닙니다.");
+
+			System.out.print("다시 찾으시겠습니까?(y/n) : ");
+			String reFind = find.nextLine();
+
+			if (reFind.equalsIgnoreCase("y")) {
+				findeUser();
+			} else if (reFind.equalsIgnoreCase("n")) {
+				Main.main(null);
+			}
+		}
+	}
+
+	private static Integer findUserInfo(Scanner find, String findName, String findJumin) throws Exception {
+
+		list = Load.loadUser();
+
+		// 체크 기본값 false 설정;
+		boolean findCheck = false;
+		
+
+		// 입력한 ID/PW 와 데이터 ID/PW 비교
+		// 같으면 true , 다르면 false
+
+		for (User s : list) {
+
+			if (s.getName().equals(findName) && s.getJumin().equals(findJumin)) {
+				findCheck = true;
+			}
+
+			
+		}
+
+		// 로그인 확인 -> 서버?로 맞는지 확인
+		if (findCheck ) {
+			return 1; // 이름과 주민번호 맞음
+		} else if (!findCheck) {
+			return 2; // 이름과 주민번호 틀림
+		
+		} else {
+			return 0; // ID/P W찾기 오류
+		}
+
+	}
+
+}
---
+++ DragonLand/src/com/project/login/login.java
@@ -0,0 +1,184 @@
+package com.project.login;
+
+import java.io.*;
+import java.util.*;
+
+import com.project.data.*;
+import com.project.main.Load;
+import com.project.main.Main;
+
+
+public class login {
+
+	private static ArrayList<User> list;
+
+	public static void main(String[] args) throws Exception {
+
+		login();
+
+	}
+
+	public static void login() throws Exception {
+
+//		System.out.println("Dragon Land"); //임시
+//		System.out.printf("🎈오늘 용용랜드는 %s합니다.🎈%n%n", Main.getState());
+		Scanner login = new Scanner(System.in);
+		String loginId;
+		String loginPw;
+
+		// 로그인 메뉴
+
+		System.out.println("              =============================");
+		System.out.println("                           로그인");
+		System.out.println("              =============================");
+		System.out.println("                  (b를 누르면 뒤로 이동합니다.)");
+
+		System.out.print("                         아이디: ");
+		loginId = login.nextLine();
+		
+
+	
+		System.out.print("                        비밀번호: ");
+		loginPw = login.nextLine();
+		
+		
+		if (loginId.equalsIgnoreCase("b")) {
+			Main.main(null); //뒤로가기(메인메뉴)
+		}
+		
+		if (loginId.equalsIgnoreCase("b")) {
+			Main.main(null); //뒤로가기(메인메뉴)
+		}
+		
+		// 관리자 로그인
+		if(loginId.equals("admin") && loginPw.equals("1234")) {
+			Main.main(null); //관리자 페이지
+		}
+		
+		int loginCheck = userLoginCheck(login, loginId, loginPw);
+
+	
+		if (loginCheck == 1) {
+
+		
+			// 로그인 확인 후 메뉴 출력
+			Main.main(null); // 회원 페이지 이동 
+
+			// 2번 비밀번호 틀림
+		} else if (loginCheck == 2) {
+
+			// 아이디 O 비밀번호 X
+			System.out.println("비밀번호가 틀렸습니다.");
+
+			System.out.print("다시 로그인 하시겠습니까?(y/n) : ");
+			String relogin = login.nextLine();
+
+			if (relogin.equals("y")) {
+
+				System.out.println("다시 시작합니다.");
+				login();
+			} else if (relogin.equals("n")) {
+				Main.main(null);
+			}
+
+			// 3번. 아이디 틀림
+		} else if (loginCheck == 3) {
+
+			// 아이디 X 비밀번호 O
+			System.out.println("아이디가 틀렸습니다.");
+
+			System.out.print("다시 로그인 하시겠습니까?(y/n) : ");
+			String relogin = login.nextLine();
+
+			if (relogin.equals("y")) {
+
+				System.out.println("다시 시작합니다.");
+				login();
+			} else if (relogin.equals("n")) {
+				Main.main(null);
+			}
+
+			// 4번. 아이디 비밀번호 모두 틀림
+		} else if (loginCheck == 4) {
+
+			// 아이디 X 비밀번호 X
+			System.out.println("아이디와 비밀번호 모두 틀렸습니다.");
+
+			System.out.print("다시 로그인 하시겠습니까?(y/n) : ");
+			String relogin = login.nextLine();
+
+			if (relogin.equals("y")) {
+
+				System.out.println("다시 시작합니다.");
+				login();
+			} else if (relogin.equals("n")) {
+				Main.main(null);
+			}
+			
+			
+		} 
+		else {
+			System.out.println("로그인 오류");
+
+			System.out.print("다시 로그인 하시겠습니까?(y/n) : ");
+			String relogin = login.nextLine();
+
+			if (relogin.equals("y")) {
+
+				System.out.println("다시 시작합니다.");
+				login();
+			} else if (relogin.equals("n")) {
+				Main.main(null);
+			}
+		}
+		
+	
+	}
+
+	
+	private static Integer userLoginCheck(Scanner login, String loginId, String loginPw) throws Exception {
+
+		// 유저 데이터 파일 불러오기
+		list = Load.loadUser();
+
+		// 체크 기본값 false 설정;
+		boolean loginIdCheck = false;
+		boolean loginPWCheck = false;
+
+		// 입력한 ID/PW 와 데이터 ID/PW 비교
+		// 같으면 true , 다르면 false
+
+		for (User s : list) {
+
+			if (s.getId().equals(loginId)) {
+				loginIdCheck = true;
+			}
+
+			if (s.getPw().equals(loginPw)) {
+				loginPWCheck = true;
+
+			}
+
+			
+		} 
+
+		// 로그인 확인 -> 서버?로 맞는지 확인
+		if (loginIdCheck && loginPWCheck) {
+			return 1; // 로그인 성공
+		} else if (loginIdCheck && !loginPWCheck) {
+			return 2; // 비밀번호 틀림
+		} else if (!loginIdCheck && loginPWCheck) {
+			return 3; // 아이디 틀림
+		} else if (!loginIdCheck && !loginPWCheck) {
+			return 4; // 둘 다 틀림
+		} else {
+			return 0; // 로그인 오류
+		}
+
+		// 체크 기본값 false 설정;
+
+	
+	}
+
+	
+}
